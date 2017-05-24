13d33bc86b2035b1762d4c627c1bfcb94fac36ad
geocachecs <gharter@csumb.edu>
2017-05-24 19:55:39 -0400

    Starting to add functionality for counting number of backups

--- backer.sh
+++ backer.sh
@@ -2,6 +2,13 @@
 
 currentWorkingDirectory=$PWD
 cd /
+
+numberOfValidFiles=$(ls /var/demobackup | awk 'BEGIN {count=0} { if(0 == system("date -d " $0 " >/dev/null")){count++;} } END {print(count)}')
+
+
+
+
+
 thisDate=$(date | awk '{printf "%s-%s-%s-%s",$1,$2,$3,$6}')
 mkdir -p /var/demobackup/${thisDate}
 tar -cf var/demobackup/${thisDate}/demo.tar root/demo
