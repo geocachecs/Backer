f9951092b92c0fcf33930388a67c07ecb46e6a6d
geocachecs <gharter@csumb.edu>
2017-05-24 20:35:43 -0400

    Added functionality so script will delete oldest backup if there are more than 6. Not yet tested

--- backer.sh
+++ backer.sh
@@ -1,10 +1,28 @@
-#! /bin/bash
+#!/bin/bash
 
 currentWorkingDirectory=$PWD
 cd /
 
 numberOfValidFiles=$(ls /var/demobackup | awk 'BEGIN {count=0} { if(0 == system("date -d " $0 " >/dev/null")){count++;} } END {print(count)}')
 
+
+if (( $numberOfValidFiles > 6 )); then
+	validFiles=(`ls /var/demobackup | awk '{if(0 == system("date -d " $0 ">/dev/null"))print($0)}'`)
+	lowest="zero"
+	for i in "${validFiles[@]}"; do
+		if (( lowest == "zero" )); then
+			lowest=$i;
+			tempold=$(stat -c %Y /var/demobackup/$lowest);
+		else
+			tempnew=$(stat -c %Y /var/demobackup/$i);
+			if (( tempnew < tempold )); then
+				lowest=$i;
+				tempold=$tempnew;
+			fi
+		fi
+	done
+	rm /var/demobackup/$lowest
+fi
 
 
 
